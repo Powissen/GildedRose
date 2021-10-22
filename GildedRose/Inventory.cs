d90bfa84f428ed219fe8d9aefae6e34e0af4ca4e
Lukáš Lokša <lukasloksa2@gmail.com>
2021-10-22 10:33:09 +0200

    Function for innkeeper for management of Quality and SellIn days of his goods

--- GildedRose/Inventory.cs
+++ GildedRose/Inventory.cs
@@ -22,8 +22,89 @@ namespace GildedRose
         /// </summary>
         public void UpdateQuality()
         {
-            // TODO ...
-            // Hint: Iterate through this.items and check Name property to access specific item
+
+            foreach (var item in items)
+            {
+                if (item.Name != "Sulfuras, Hand of Ragnaros")
+                {
+                    item.SellIn--;
+                }
+            }
+
+            foreach (var item in items)
+            {
+                if (item.Name == "Sulfuras, Hand of Ragnaros") {}
+                else
+                {
+                    if (item.Name == "Aged Brie")
+                    {
+                        if (item.SellIn > 0)
+                        {
+                            item.Quality++;
+                        }
+                        else
+                        {
+                            item.Quality += 2;
+                        }
+                    }
+                    else
+                    {
+                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            if (item.SellIn <= 0)
+                            {
+                                item.Quality = 0;
+                            }
+                            else if (item.SellIn <= 5 && item.SellIn > 0)
+                            {
+                                item.Quality += 3;
+                            }
+                            else if (item.SellIn <= 10 && item.SellIn > 5)
+                            {
+                                item.Quality += 2;
+                            }
+                            else
+                            {
+                                item.Quality++;
+                            }
+                        }
+                        else
+                        {
+                            if (item.Name.Contains("Conjured") && item.SellIn > 0)
+                            {
+                                item.Quality -= 2;
+                            }
+                            else if (item.Name.Contains("Conjured") && item.SellIn <= 0)
+                            {
+                                item.Quality -= 4;
+                            }
+                            else
+                            {
+                                if (item.SellIn <= 0)
+                                {
+                                    item.Quality -= 2;
+                                }
+                                else
+                                {
+                                    item.Quality -= 1;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (item.Quality < 0)
+                {
+                    item.Quality = 0;
+                }
+
+                if (item.Quality > 50 && item.Name != "Sulfuras, Hand of Ragnaros")
+                {
+                    item.Quality = 50;
+                }
+
+            }
         }
     }
 }
+
